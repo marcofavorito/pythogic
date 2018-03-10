984b2bbaac9a23e3317fafd6400d42cc051263b6
Marco Favorito <marco.favorito@gmail.com>
2018-03-10 21:19:52 +0100

    Bump version: 0.2.7 → 0.2.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='pythogic',
-    version='0.2.7',
+    version='0.2.8',
     description="Python package for deal with logical formulas and formal systems",
     long_description=readme + '\n\n' + history,
     author="Marco Favorito",
